c3293f98b92336ab002b687fc4aecf01a932a586
bruychik <artembruy@gmail.com>
2020-04-12 18:25:41 +0300

    I did it

---
+++ OS_2/mainwindow.cpp
@@ -0,0 +1,47 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent):
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QFileDialog dialog;
+    QStringList fileNames;
+
+    if (dialog.exec())
+    {
+        fileNames = dialog.selectedFiles();
+        ui->textEdit_2->setText(fileNames[0]);
+
+        QFile file(fileNames[0]);
+
+        if (file.open(QIODevice::ReadOnly))
+        {
+            ui->textEdit->setPlainText(QString::fromUtf8(file.readAll()));
+        }
+        file.close();
+    }
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QFile file(ui->textEdit_2->toPlainText());
+
+    if (file.open(QIODevice::WriteOnly))
+    {
+        file.write(ui->textEdit->toPlainText().toUtf8());
+        file.close();
+    }
+}
---
+++ OS_2/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QDialog>
+#include <QFileDialog>
+#include <QStringList>
+#include <QStringList>
+#include <QTextDocumentWriter>
+#include <QTextEdit>
+#include <QString>
+#include <QTextStream>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
